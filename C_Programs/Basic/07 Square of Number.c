9b99026b8edac4e26eabd002b1641fd2b66e23a8
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-04-14 22:53:10 +0630

    Practice

---
+++ C_Programs/Basic/07 Square of Number.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0,Square = 0;
+
+    printf("\n Enter Any Number =>");
+    scanf("%d",&No);
+
+    Square = No * No;
+
+    printf("\n Square of Given Number is = %d",Square);
+
+    getch();
+    return 0;
+}
