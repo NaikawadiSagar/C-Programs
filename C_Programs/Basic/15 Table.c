b32ca0ffa9040e8e6812796782e98536ca9ad73d
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-04-16 14:31:31 +0630

    Practice

---
+++ C_Programs/Basic/15 Table.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int R,C;
+
+    printf("\n Enter Any Number => ");
+    scanf("%d",&Num);
+
+    for( i = 1;i<=10;i++)
+    {
+        for(SNum = 0;ENum<)
+    }
+    getch();
+    return 0;
+}
