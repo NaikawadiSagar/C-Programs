ffaaa4e22cc1f94cc508aac05bd13803d356be09
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-04-20 21:56:02 +0630

    Practice

---
+++ C_Programs/Digit_Programs/03 Program To Calculate Even Digit In Given Number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0,Ecnt = 0,Dig = 0;
+
+    printf("\n Enter Any Positive Integer Number => ");
+    scanf("%d",&No);
+
+    while(No > 0)
+    {
+        Dig = No % 10;
+        if(Dig % 2 == 0)
+        {
+            Ecnt++;
+        }
+        No = No / 10;
+    }
+    printf("\n Even Count In Given Number is %d",Ecnt);
+    getch();
+    return 0;
+}
