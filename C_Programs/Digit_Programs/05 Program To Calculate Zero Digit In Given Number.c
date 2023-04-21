d8c020930f7aa84e41904b24172db4f397582763
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-04-21 21:50:44 +0630

    practice

---
+++ C_Programs/Digit_Programs/05 Program To Calculate Zero Digit In Given Number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int No = 0,Ocnt = 0,Dig = 0;
+
+    printf("\n Enter Any Positive Integer Number => ");
+    scanf("%d",&No);
+
+    while(No > 0)
+    {
+        Dig = No % 10;
+        if(Dig == 0)
+        {
+            Ocnt++;
+        }
+        No = No / 10;
+    }
+    printf("\n Even Count In Given Number is %d",Ocnt);
+    getch();
+    return 0;
+}
