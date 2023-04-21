71bb0022766414015d87fe1493aeac8ee19553e5
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-04-21 21:49:56 +0630

    Practice

---
+++ C_Programs/Basic/21 Reverse Number Printing.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i = 0,No = 10;
+
+    for(i = 10;i >=1 ;i--)
+    {
+        printf("\n %d",i);
+    }
+    getch();
+    return 0;
+}
