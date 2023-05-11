3dae4aecbfb370f59f2cc0e4949e1a9d2df9b0ad
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-05-11 07:58:59 +0630

    Practice

---
+++ C_Programs/Array_Programs/03 Display Array Elements.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Arr[5] = {5,7,3,5,7,3};
+
+
+    printf("\n First Element is  = %d",Arr[0]);
+    printf("\n Second Element is = %d",Arr[1]);
+    printf("\n Third Element is  = %d",Arr[2]);
+    printf("\n Fourth Element is = %d",Arr[3]);
+    printf("\n Fifth Element is  = %d",Arr[4]);
+
+    getch();
+    return 0;
+}
