799b5520243e842bc591bfc3cefb736d50984cc2
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-05-15 07:30:24 +0630

    Practice

---
+++ C_Programs/Array_Programs/16 Create Array Withinitilization & Display All Elements.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Arr[5] ={};
+
+    printf("\n 1st Element is %d",Arr[0]);
+    printf("\n 2nd Element is %d",Arr[1]);
+    printf("\n 3rd Element is %d",Arr[2]);
+    printf("\n 4th Element is %d",Arr[3]);
+    printf("\n 5th Element is %d",Arr[4]);
+
+    getch();
+    return 0;
+
+}
