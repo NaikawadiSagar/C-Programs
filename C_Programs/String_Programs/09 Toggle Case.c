7a18823078013a5a218d10610ed3ab338ee45071
NaikawadiSagar <sagarnaikawadi2605@gmail.com>
2023-06-07 12:48:54 +0630

    Practice

---
+++ C_Programs/String_Programs/09 Toggle Case.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    char cSrc[60] = "";
+    int i = 0;
+
+    printf("\n Enter A String => ");
+    gets(cSrc);
+
+    while(cSrc[i] != '\0')
+    {
+        if(cSrc[i] >= 'A' && cSrc[i] <= 'Z')
+        {
+            cSrc[i] = cSrc[i] + 32;
+        }
+
+        else if(cSrc[i] >= 'a' && cSrc[i] <= 'z')
+        {
+            cSrc[i] = cSrc[i] - 32;
+        }
+        i++;
+    }
+    printf("\n Toggle Case is %s",cSrc);
+
+    getch();
+    return 0;
+}
